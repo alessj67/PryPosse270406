f338682dc3045c117c08351d77e5299919001440
alessj67 <alegraposse@hotmail.com>
2026-05-10 23:11:37 -0300

    .

--- PryPosse270406/FrmBoleteria.Designer.cs
+++ PryPosse270406/FrmBoleteria.Designer.cs
@@ -117,6 +117,7 @@
             // 
             // FrmBoleteria
             // 
+            this.AcceptButton = this.btnCalcular;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
@@ -129,6 +130,7 @@
             this.Controls.Add(this.lblDistancia);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmBoleteria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Boletería";
             this.ResumeLayout(false);
             this.PerformLayout();
